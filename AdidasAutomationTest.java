03012e2e8b3a45077198cf2b83a500d55348b62f
aleksandralukic <40288254+aleksandralukic@users.noreply.github.com>
2018-07-07 15:11:03 +0200

    Add files via upload

---
+++ AdidasAutomationTest.java
@@ -0,0 +1,47 @@
+package adidas;
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AdidasAutomationTest {
+
+	public static void main(String[] args) {
+	
+			
+				System.setProperty("webdriver.chrome.driver","/Users/aleksandralukic/chromedriver");
+				WebDriver driver = new ChromeDriver();
+				String commentTextActual = "Sending a comment with a correct email in testing purposes";
+				driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+				driver.get("http://store.demoqa.com/");
+				driver.findElement(By.id("menu-item-54")).click();
+				driver.findElement(By.id("comment")).sendKeys("I am going to send a comment with wrong email");
+				driver.findElement(By.id("author")).sendKeys("Aleksandra");
+				driver.findElement(By.id("email")).sendKeys("testmailwrong.com");
+				driver.findElement(By.id("submit")).click();
+				if(driver.findElement(By.id("error-page")).isDisplayed()) {
+					System.out.println("Error is displayed");
+				}
+				else {
+					System.out.println("Error is not displayed");
+				}
+				driver.findElement(By.tagName("a")).click();
+				driver.findElement(By.id("menu-item-54")).click();
+				driver.findElement(By.id("comment")).sendKeys(commentTextActual);
+				driver.findElement(By.id("author")).sendKeys("Aleksandra");
+				driver.findElement(By.id("email")).sendKeys("a.lukic@piratetech.com");
+				driver.findElement(By.id("submit")).click();
+				WebElement commentBody = driver.findElement(By.className("commentlist"));
+				String commentText = commentBody.getText();
+				if(commentText.contains(commentTextActual))
+					System.out.println("Comment succesfully submited");
+				else {
+					System.out.println("Comment is not submitted");
+				}
+				driver.close();
+				
+				
+	}
+}
